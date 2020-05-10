d1e5f948b5d13416579773df6ac6f6e318c47748
Monkeydew <55723201+Calioses@users.noreply.github.com>
2020-05-09 21:52:33 -0400

    This bug is beyond me

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,3 +1,4 @@
+/*
 import * as React from "react";
 
 
@@ -23,16 +24,15 @@ export interface IFormState {
 }
 
 export class Form extends React.Component<IFormProps, IFormState> {
-    submitForm(): boolean | PromiseLike<boolean> {
-        throw new Error("Method not implemented.");
+    //submitForm(): boolean | PromiseLike<boolean> {
+    //    throw new Error("Method not implemented.");
     }
     constructor(props: IFormProps) {
         super(props);
 
         const errors: IErrors = {};
-
-    const values: IValues = {};
-    this.state = {
+        const values: IValues = {};
+        this.state = {
         errors,
         values
     };
@@ -41,7 +41,6 @@ export class Form extends React.Component<IFormProps, IFormState> {
 
 private haveErrors(errors: IErrors) {
     let haveError: boolean = false;
-
 Object.keys(errors).map((key: string) => {
     if (errors[key].length > 0) {
         haveError = true;
@@ -103,8 +102,7 @@ public render() {
         </div>
       </form>
     );
-  }
-}    
+  */
                 
                 
                 
@@ -120,3 +118,132 @@ public render() {
 
 
     
+
+import * as React from "react";
+
+interface IFormProps {
+  /* The http path that the form will be posted to */
+  action: string;
+
+  render: () => React.ReactNode;
+}
+
+export interface IValues {
+  /* Key value pairs for all the field values with key being the field name */
+  [key: string]: any;
+}
+
+export interface IErrors {
+  /* The validation error messages for each field (key is the field name */
+  [key: string]: string;
+}
+
+export interface IFormState {
+  /* The field values */
+  values: IValues;
+
+  /* The field validation error messages */
+  errors: IErrors;
+
+  /* Whether the form has been successfully submitted */
+  submitSuccess?: boolean;
+}
+
+export class Form extends React.Component<IFormProps, IFormState> {
+  constructor(props: IFormProps) {
+    super(props);
+
+    const errors: IErrors = {};
+    const values: IValues = {};
+    this.state = {
+      errors,
+      values
+    };
+  }
+
+  /**
+   * Returns whether there are any errors in the errors object that is passed in
+   * @param {IErrors} errors - The field errors
+   */
+  private haveErrors(errors: IErrors) {
+    let haveError: boolean = false;
+    Object.keys(errors).map((key: string) => {
+      if (errors[key].length > 0) {
+        haveError = true;
+      }
+    });
+    return haveError;
+  }
+
+  /**
+   * Handles form submission
+   * @param {React.FormEvent<HTMLFormElement>} e - The form event
+   */
+  private handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    e.preventDefault();
+
+    if (this.validateForm()) {
+      const submitSuccess: boolean = await this.submitForm();
+      this.setState({ submitSuccess });
+    }
+  };
+
+  /**
+   * Executes the validation rules for all the fields on the form and sets the error state
+   * @returns {boolean} - Whether the form is valid or not
+   */
+  private validateForm(): boolean {
+    // TODO - validate form
+    return true;
+  }
+
+  /**
+   * Submits the form to the http api
+   * @returns {boolean} - Whether the form submission was successful or not
+   */
+  private async submitForm(): Promise<boolean> {
+    // TODO - submit the form
+    return true;
+  }
+
+  public render() {
+    const { submitSuccess, errors } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit} noValidate={true}>
+        <div className="container">
+  
+          {this.props.render()}
+  
+          <div className="form-group">
+            <button
+              type="submit"
+              className="btn btn-primary"
+              disabled={this.haveErrors(errors)}
+          >
+            Submit
+          </button>
+        </div>
+          {submitSuccess && (
+            <div className="alert alert-info" role="alert">
+              The form was successfully submitted!
+            </div>
+          )}
+          {submitSuccess === false &&
+            !this.haveErrors(errors) && (
+              <div className="alert alert-danger" role="alert">
+                Sorry, an unexpected error has occurred
+              </div>
+            )}
+          {submitSuccess === false &&
+            this.haveErrors(errors) && (
+              <div className="alert alert-danger" role="alert">
+                Sorry, the form is invalid. Please review, adjust and try again
+              </div>
+            )}
+        </div>
+      </form>
+    );
+  }
+}
