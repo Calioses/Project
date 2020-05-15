5c29695702e6c545f12a2a339c178ce59e8efd70
Monkeydew <55723201+Calioses@users.noreply.github.com>
2020-05-15 16:38:55 -0400

    working

--- src/Views/ContactUsForm.tsx
+++ src/Views/ContactUsForm.tsx
@@ -1,36 +1,36 @@
-// import * as React from "react";
-// import { Form } from "./Form";
-// import { Field } from "./Field";
-
-// export const ContactUsForm: React.SFC = () => {
-//     return (
-//         <Form
-//             action="http://localhost:3000"
-//             render={() => (
-//                 <React.Fragment>
-//                     <div className="alert alert-info" role="alert">
-//                     Enter the information below and we'll get back to you as soon as we can.
-//                     </div>
-//                     <Field id="name" label="Name" />
-//                     <Field id="email" label="Email" />
-//                     <Field 
-//                     id="reason"
-//                     label="Reason"
-//                     editor="dropdown"
-//                     options={["", "Marketing", "Support", "Feedback","Jobs"]}
-//                     />
-//                     <Field id="notes" label="Notes" editor="multilinetextbox" />
-//                     </React.Fragment>
-//             )}
-//             />
-//     );
-// };
 
 import React,{Component} from "react";
 import {Form} from "reactstrap";
 import Field from "../Components/Field";
+import { Input } from "reactstrap";
+import { FormGroup } from "reactstrap";
+import { observable, action, } from "mobx";
+import { observer } from "mobx-react";
 
+@observer
 export default class ContactUsForm extends Component  {
+  @observable name: string = "";
+  @observable email: string = "";  
+  @observable reason: string = ""; 
+  @observable notes: string = ""; 
+ 
+  @action
+  handleType = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const id: string | null = e.currentTarget.getAttribute("id");
+    if(id==="name"){
+      this.name=e.currentTarget.value;
+    }
+    else if(id==="email"){
+      this.email=e.currentTarget.value;
+    }
+    else if(id==="reason"){
+      this.reason=e.currentTarget.value;
+    }
+    else if(id==="notes"){
+      this.notes=e.currentTarget.value;
+    }
+  }
+
  render(){
   return (
     <Form>
@@ -39,18 +39,63 @@ export default class ContactUsForm extends Component  {
             Enter the information below and we'll get back to you as soon as we
             can.
           </div>
-          <Field id="name" label="Name" />
-          <Field id="email" label="Email" />
-          <Field
+
+          <FormGroup>
+          <label>
+            Name:
+          </label>
+          <Input 
+            id="name" 
+            name="name" 
+            type="text"
+            onChange={this.handleType}
+            value={this.name}
+            />
+          </FormGroup>
+
+          <FormGroup>
+          <label>
+            Email:
+          </label>
+          <Input 
+            id="email"
+            name="email"
+            type="text"
+            onChange={this.handleType}
+            value={this.email}
+            />
+          </FormGroup>
+          
+          <FormGroup>
+          <label>
+            Reason:
+          </label>
+          <Input
             id="reason"
-            label="Reason"
-            editor="dropdown"
+            name="reason" 
+            onChange={this.handleType}
             options={["", "Marketing", "Support", "Feedback", "Jobs"]}
-          />
-          <Field id="notes" label="Notes" editor="multilinetextbox" />
-        
+            value={this.reason}
+            type="text"
+            />
+          </FormGroup>
+
+          <FormGroup>
+          <label>
+            Notes:          
+          </label>
+
+          <Input
+            type="text"
+            id="notes"
+            name="notes" 
+            onChange={this.handleType}
+            value={this.notes} 
+            />
+          </FormGroup>
+
         </Form>
   );
  }
-  
+ 
 };
